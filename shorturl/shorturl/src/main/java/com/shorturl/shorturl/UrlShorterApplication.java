4cbe97bf13a33c87cb8e499e5681babfe8bb5a49
robiarth <arthur.prevot@epitech.eu>
2025-07-02 00:48:32 +0200

    [ADD] Project initialization

---
+++ shorturl/shorturl/src/main/java/com/shorturl/shorturl/UrlShorterApplication.java
@@ -0,0 +1,13 @@
+package com.shorturl.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorterApplication.class, args);
+	}
+
+}
